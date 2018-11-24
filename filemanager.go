80d0bf66c991274d21815576c4b8ee83cc7d24bd
pantheonsh <pantheonshell@yahoo.com>
2018-11-24 16:46:36 -0200

    função de listar diretórios

--- filemanager.go
+++ filemanager.go
@@ -1,5 +1,7 @@
 package main
 
+import "io/ioutil"
+
 // FileManager type
 type FileManager struct {
 	CurrPath string
@@ -13,5 +15,21 @@ type File struct {
 }
 
 func (fm FileManager) getDirectoryListing() []File {
-	return nil
+	files, err := ioutil.ReadDir(fm.CurrPath)
+	var rfiles = []File{}
+
+	if err != nil {
+		return nil
+	}
+
+	for _, elem := range files {
+		file := File{
+			Name: "oi",
+			Path: "d",
+			Size: elem.Size(),
+		}
+		_ = append(rfiles, file)
+	}
+
+	return rfiles
 }
